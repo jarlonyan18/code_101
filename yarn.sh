dcba402ac97b5057ccaceb19d0e5de005ae6faf4
jarlonyan18 <jarlonyan18@outlook.com>
2025-07-17 17:07:44 +0800

    Create yarn.sh

---
+++ yarn.sh
@@ -0,0 +1,9 @@
+#列出来yarn任务
+yarn application -list
+
+
+#拉取yarn任务的日志
+yarn logs -applicationId application_xxxx
+
+
+yarn logs -applicationId application_xxx -containerId container_xxx -nodeAddress xxxx
