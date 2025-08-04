9234a4f506c4e941d2a297a0cc88295f2675383c
jarlonyan18 <jarlonyan18@outlook.com>
2025-08-04 11:36:40 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,12 @@ docker pull xxxxx
 
 
 #启动镜像
-docker run -d -it /bin/bash -e xxx - yyy --name zhousc66 -p 7788:8877 -v ~/zzzz:/data
+docker run -itd --name [容器名字] [镜像名字:镜像tag] /bin/bash
+-d 会将容器保持后台运行，否则执行/bin/bash的话会立即执行完毕导致容器退出
 
-docker exec -it xxxxxx bash
+#查看容器列表
+docker ps -a
+
+#进入一个容器，开始交互
+docker exec -it [短ID] /bin/bash
 
